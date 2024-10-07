3bf2188a4987794401ce3dbae7d9cf5c3542f6f8
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-10-07 21:17:08 +0530

    Add Sky.jsx new code

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -20,8 +20,8 @@ export function Sky({ isRotating }) {
 
   return (
     <mesh ref={skyRef}>
-      // use the primitive element when you want to directly embed a complex 3D
-      model or scene
+      {/* // use the primitive element when you want to directly embed a complex 3D
+      model or scene */}
       <primitive object={sky.scene} />
     </mesh>
   );
