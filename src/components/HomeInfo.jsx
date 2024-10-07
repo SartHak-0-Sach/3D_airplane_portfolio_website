97d052f45ec3bcc54def05cacb2f1667d6fe92d1
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-10-07 21:15:07 +0530

    Commit HomeInfo.jsx

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -7,10 +7,10 @@ const HomeInfo = ({ currentStage }) => {
     return (
       <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
         Hi, I'm
-        <span className='font-semibold mx-2 text-white'>Adrian</span>
+        <span className='font-semibold mx-2 text-white'>Sarthak</span>
         👋
         <br />
-        A Software Engineer from Croatia 🇭🇷
+        An aspiring Software Engineer from India
       </h1>
     );
 
@@ -18,7 +18,7 @@ const HomeInfo = ({ currentStage }) => {
     return (
       <div className='info-box'>
         <p className='font-medium sm:text-xl text-center'>
-          Worked with many companies <br /> and picked up many skills along the way
+          Worked with many people <br /> and picked up many skills along the way
         </p>
 
         <Link to='/about' className='neo-brutalism-white neo-btn'>
