0951a6c9b465e9f6dc678d041c2cae6acbaf95e4
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-10-07 21:14:34 +0530

    Update CTA.jsx

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
     <section className='cta'>
       <p className='cta-text'>
         Have a project in mind? <br className='sm:block hidden' />
-        Let’s build something together!
+        Let's build something together!
       </p>
       <Link to='/contact' className='btn'>
         Contact
