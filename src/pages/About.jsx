ed30c438604c756b45d6114d3f1117ccd91b18e1
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-10-07 21:17:46 +0530

    Update about section information

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -22,8 +22,7 @@ const About = () => {
 
       <div className='mt-5 flex flex-col gap-3 text-slate-500'>
         <p>
-          Software Engineer based in Croatia, specializing in technical
-          education through hands-on learning and building applications.
+          Software Engineer based in India, specializing in technical education through hands-on learning and building applications.
         </p>
       </div>
 
