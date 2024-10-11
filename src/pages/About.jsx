aadcb1fdec6cc9a0c5c5e275fc1fac9b88da67a1
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-10-11 17:07:18 +0530

    Fix social links

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
         Hello, I'm{" "}
         <span className='blue-gradient_text font-semibold drop-shadow'>
           {" "}
-          Adrian
+          Sarthak
         </span>{" "}
         👋
       </h1>
